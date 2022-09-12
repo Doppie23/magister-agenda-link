af32aaa41039e9145e3704152489a06490a75d83
Doppie22 <dvopstal05@gmail.com>
2022-09-12 18:14:01 +0200

    iets

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
-import logging
-import os
-from datetime import date, datetime
-from pathlib import Path
-from typing import Optional
-import urllib.request
-from urllib.request import urlopen 
+link = r'https://calendar.magister.net/api/icalendar/feeds/5bd9ac88-6e89-428c-9870-233085ef6636'
 
-import fire
-import pandas as pd
-from ics import Calendar
+import requests
 
-link = 'webcal://calendar.magister.net/api/icalendar/feeds/5bd9ac88-6e89-428c-9870-233085ef6636'
+text = requests.get(link).text
 
-def main():
-    req = urllib.request('webcal://calendar.magister.net/api/icalendar/feeds/5bd9ac88-6e89-428c-9870-233085ef6636')
-    response = urlopen(req)
-    data = response.read()
-
-    cal = Calendar.from_ical(data)
-
-    for event in cal.walk('vevent'):
-
-        date = event.get('dtstart')
-        summery = event.get('summary')
-
-        print(str(date))
-        print(str(summery))
-
-    return
-
-main()
-
+print(text)

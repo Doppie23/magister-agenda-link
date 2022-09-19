945d58d0b516148c5955a52abbcc05c5616180b8
Doppie22 <dvopstal05@gmail.com>
2022-09-19 21:39:36 +0200

    kan nu in ander functie getal zetten

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import os
 from datetime import date, timedelta, datetime
 
 class magister():
-    def __init__(self, dagenverschil=None):
+    def __init__(self):
         url = os.environ.get('link')
         self.c = Calendar(urlopen(url).read().decode('utf-8'))
         self.e = list(self.c.timeline)
-        self.dagenverschil = dagenverschil
 
-    def anderedag(self):
+    def anderedag(self, dagenverschil=None):
+        self.dagenverschil = dagenverschil
         vandaag = str(date.today())
         if self.dagenverschil == None:
             dag = vandaag
@@ -21,9 +21,8 @@ class magister():
             dag = vandaag.strftime("%Y-%M-%d")
         return dag
 
-    def printdag(self):
-        self.dag = magister.anderedag(self)
-
+    def printdag(self, dagenverschil=None):
+        self.dag = magister.anderedag(self, dagenverschil)
         for x in self.e:
             if self.dag in str(x._begin):
                 print(x.name)
@@ -38,4 +37,4 @@ class magister():
                 continue
 
 agenda = magister()
-agenda.printdag()
+agenda.printdag() # kan tussen haakjes 1 zetten voor volgende dag en 2 voor dag erna etc...

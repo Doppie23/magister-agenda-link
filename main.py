e6f6696e99c8d09721d009bc6c3c4151005c97af
Doppie22 <dvopstal05@gmail.com>
2022-09-19 18:04:10 +0200

    correcte tijdzone

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from urllib.request import urlopen
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from datetime import date
+from datetime import date, timedelta, datetime
 
 url = os.environ.get('link')
 c = Calendar(urlopen(url).read().decode('utf-8'))
 e = list(c.timeline)
 vandaag = str(date.today())
 
-print(vandaag)
+# print(vandaag)
 
 for x in e:
     if vandaag in str(x._begin):
         print(x.name)
-        print(x._begin)
+        datumtijd = map(str,str(x._begin).split('T'))
+        datum, tijd = datumtijd
+        tijd = map(str,str(tijd).split('+'))
+        tijd, _ = tijd
+        tijdzone_correctie = datetime.strptime(tijd, "%H:%M:%S") + timedelta(hours=2)
+        tijd = tijdzone_correctie.strftime("%H:%M:%S")
+        print(tijd)
     else:
         continue

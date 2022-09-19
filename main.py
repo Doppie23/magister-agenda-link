56e7101bd34d9a97155b164f4388fd6c9d53e26c
Doppie22 <dvopstal05@gmail.com>
2022-09-19 18:32:58 +0200

    kan nu ook ander dagne zien

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ from datetime import date, timedelta, datetime
 url = os.environ.get('link')
 c = Calendar(urlopen(url).read().decode('utf-8'))
 e = list(c.timeline)
-vandaag = str(date.today())
 
-# print(vandaag)
+def anderedag(dagenverschil=None):
+    vandaag = str(date.today())
+    if dagenverschil == None:
+        dag = vandaag
+    else:
+        vandaag = datetime.strptime(vandaag, "%Y-%M-%d") + timedelta(days=dagenverschil)
+        dag = vandaag.strftime("%Y-%M-%d")
+    return dag
+
+dag = anderedag()
 
 for x in e:
-    if vandaag in str(x._begin):
+    if dag in str(x._begin):
         print(x.name)
-        datumtijd = map(str,str(x._begin).split('T'))
+        datumtijd = str(x._begin).split('T')
         datum, tijd = datumtijd
-        tijd = map(str,str(tijd).split('+'))
+        tijd = str(tijd).split('+')
         tijd, _ = tijd
         tijdzone_correctie = datetime.strptime(tijd, "%H:%M:%S") + timedelta(hours=2)
         tijd = tijdzone_correctie.strftime("%H:%M:%S")

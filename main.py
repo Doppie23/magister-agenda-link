2bd1166e045a493cef3f1b20ba86389e1aee2c1d
Doppie22 <dvopstal05@gmail.com>
2022-09-19 16:53:05 +0200

    backup

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from urllib.request import urlopen
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from datetime import date
 
 url = os.environ.get('link')
 c = Calendar(urlopen(url).read().decode('utf-8'))
 e = list(c.timeline)
+vandaag = date.today()
 
+print(vandaag)
+
+print(e[0].name)
 print(e[0]._begin)
+
+print(e[4].name)
+print(e[4]._begin)

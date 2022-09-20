3a9573af62875fb93a4b9e03566e324f800ad067
Doppie22 <dvopstal05@gmail.com>
2022-09-20 13:50:32 +0200

    eerste uur en rooster nu niet meer naar console

--- main.py
+++ main.py
@@ -5,36 +5,59 @@ load_dotenv()
 import os
 from datetime import date, timedelta, datetime
 
-class magister():
+class magisteragenda():
     def __init__(self):
         url = os.environ.get('link')
         self.c = Calendar(urlopen(url).read().decode('utf-8'))
         self.e = list(self.c.timeline)
+        self.vandaag = str(date.today())
 
     def anderedag(self, dagenverschil=None):
         self.dagenverschil = dagenverschil
-        vandaag = str(date.today())
-        if self.dagenverschil == None:
+        vandaag = self.vandaag
+        if self.dagenverschil == None or 0:
             dag = vandaag
         else:
             vandaag = datetime.strptime(vandaag, "%Y-%M-%d") + timedelta(days=self.dagenverschil)
             dag = vandaag.strftime("%Y-%M-%d")
         return dag
 
-    def printdag(self, dagenverschil=None):
-        self.dag = magister.anderedag(self, dagenverschil)
+    def rooster(self, dagenverschil=None):
+        rooster = ''
+        dag = magisteragenda.anderedag(self, dagenverschil)
         for x in self.e:
-            if self.dag in str(x._begin):
-                print(x.name)
+            if dag in str(x._begin):
+                rooster += str(x.name) + '\n'
                 datumtijd = str(x._begin).split('T')
                 datum, tijd = datumtijd
                 tijd = str(tijd).split('+')
                 tijd, _ = tijd
                 tijdzone_correctie = datetime.strptime(tijd, "%H:%M:%S") + timedelta(hours=2)
                 tijd = tijdzone_correctie.strftime("%H:%M:%S")
-                print(tijd)
+                rooster += str(tijd) + '\n'
             else:
                 continue
+        rooster = rooster.strip()
+        return rooster
 
-agenda = magister()
-agenda.printdag() # kan tussen haakjes 1 zetten voor volgende dag en 2 voor dag erna etc...
+    def tijd_eerste_uur(self):
+        eersteuur = None
+        i = 0
+        for x in self.e:
+            if self.vandaag in str(x._begin) and  i==0:
+                datumtijd = str(x._begin).split('T')
+                _, tijd = datumtijd
+                tijd = str(tijd).split('+')
+                tijd, _ = tijd
+                tijdzone_correctie = datetime.strptime(tijd, "%H:%M:%S") + timedelta(hours=2)
+                eersteuur = str(tijdzone_correctie.strftime("%H:%M:%S"))
+                i += 1 #zodat we alleen maar eerste uur van de dag pakken en niet de rest
+            else:
+                continue
+        return eersteuur
+
+agenda = magisteragenda()
+rooster = agenda.rooster() # kan tussen haakjes 1 zetten voor volgende dag en 2 voor dag erna etc...
+print(rooster)
+eersteuur = agenda.tijd_eerste_uur()
+print(eersteuur)
